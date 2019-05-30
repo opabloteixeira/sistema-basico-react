942785c95e91a4e32c68ff6086d302e404c09787
Pablo <pablopst@gmail.com>
2019-05-30 11:12:30 -0300

    Finalizado

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+import React from 'react';
+
+export default props => (
+    <footer className="footer">
+    <span>
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> e com React.JS por
+        <strong> Pablo Teixeira </strong>
+    </span>
+    </footer>
+);
---
+++ src/components/user/UserCrud.jsx
@@ -0,0 +1,181 @@
+import React, {Component} from "react";
+import axios from "axios";
+import Main from "../template/Main";
+
+const headerProps = {
+    icon: "users",
+    title: "Usuários",
+    subtitle: "Cadastro de usuários: Incluir, Listar, Alterar, Excluir"
+}
+
+const baseUrl = "http://localhost:3001/users";
+
+const initialState = {
+    user: { name: "", email: ""},
+    list: []
+}
+
+export default class UserCrud extends Component {
+    
+    state = { ...initialState }
+
+
+    /* Busca a lista pra mostrar no front */
+    componentWillMount(){
+        axios(baseUrl).then(resp => {
+            this.setState({list: resp.data});
+        });
+    }
+
+
+    clear() {
+        this.setState({user: initialState.user})
+    }
+
+
+    save() {
+        const user = this.state.user;
+        const method = user.id ? 'put' : "post";
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+
+        axios[method](url, user).then(resp =>{
+            const list = this.getUpdatedList(resp.data);
+            this.setState({user: initialState.user, list});
+        });
+        
+    }
+
+
+    getUpdatedList(user, add = true) {
+        const list = this.state.list.filter(u => u.id !== user.id);
+
+        if( add ){ list.unshift(user); } /* Se user estiver setado add no inicio */
+
+        return list;
+    }
+
+
+    updateField(e) {
+        const user = { ...this.state.user }
+        user[e.target.name] = e.target.value;
+        this.setState({ user });
+    }
+
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input 
+                                name="name" 
+                                value={this.state.user.name} 
+                                type="text" 
+                                className="form-control"
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..."
+                            />
+
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input 
+                                name="email" 
+                                value={this.state.user.email} 
+                                type="text" 
+                                className="form-control"
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o email..."
+                            />
+
+                        </div>
+                    </div>
+                </div>
+
+                <hr/>
+
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                        onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+
+                        <button className="btn btn-secondary ml-2"
+                        onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>    
+        )
+    }
+
+
+    load(user) {
+        this.setState({user});
+    }
+
+    remove(user) {
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.getUpdatedList(user, false);
+            this.setState({list});
+        });
+    }
+
+
+
+    renderTable(){
+        return(
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { this.renderRows() }
+                </tbody>
+            </table>
+        );
+    }
+
+
+    renderRows() {
+        return this.state.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning"
+                        onClick={ () => this.load(user) }>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2"
+                        onClick={ () => this.remove(user) }>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            );
+        });
+    }
+
+    render() {
+        console.log(this.state.list);
+        return (
+            <Main {...headerProps}>
+                {this.renderForm()}
+                {this.renderTable()}
+            </Main>
+        );
+    }
+}
